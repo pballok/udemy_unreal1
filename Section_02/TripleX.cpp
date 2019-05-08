88d8f8fb6c027b01110358e90310f6ccc222f44a
Peter Ballok <pballok@gmail.com>
2019-05-08 15:03:21 -0400

    Lecture 18

---
+++ Section_02/TripleX.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int, char**)
+{
+  std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
+  std::cout << "Enter the correct code to continue..." << std::endl;
+
+  return 0;
+}
