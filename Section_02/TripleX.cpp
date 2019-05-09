02fd238e41b382c5836d43c36434be85194f53fd
Peter Ballok <pballok@gmail.com>
2019-05-09 09:08:19 -0400

    Lecture 20

--- Section_02/TripleX.cpp
+++ Section_02/TripleX.cpp
@@ -5,5 +5,14 @@ int main(int, char**)
   std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
   std::cout << "Enter the correct code to continue..." << std::endl;
 
+  constexpr int a = 4;
+  constexpr int b = 3;
+  constexpr int c = 2;
+
+  constexpr int sum = a + b +c;
+  constexpr int product = a * b * c;
+
+  std::cout << sum << std::endl << product << std::endl;
+
   return 0;
 }
