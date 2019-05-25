bdf5c886de094cb08f0250367e16605fd99aa73d
Peter Ballok <pballok@gmail.com>
2019-05-25 13:27:45 -0400

    Section 27

--- Section_02/TripleX.cpp
+++ Section_02/TripleX.cpp
@@ -3,16 +3,33 @@
 int main(int, char**)
 {
   std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
-  std::cout << "Enter the correct code to continue..." << std::endl;
+  std::cout << "Enter the correct code to continue..." << std::endl << std::endl;
 
-  constexpr int a = 4;
-  constexpr int b = 3;
-  constexpr int c = 2;
+  constexpr int CodeA = 4;
+  constexpr int CodeB = 3;
+  constexpr int CodeC = 2;
 
-  constexpr int sum = a + b +c;
-  constexpr int product = a * b * c;
+  constexpr int CodeSum = CodeA + CodeB + CodeC;
+  constexpr int CodeProduct = CodeA * CodeB * CodeC;
 
-  std::cout << sum << std::endl << product << std::endl;
+  std::cout << "There three numbers in the code." << std::endl;
+  std::cout << "The codes add up to " << CodeSum << std::endl;
+  std::cout << "The codes multiply to give " << CodeProduct << std::endl;
+
+  int GuessA = 0;
+  int GuessB = 0;
+  int GuessC = 0;
+
+  std::cin >> GuessA >> GuessB >> GuessC;
+
+  int GuessSum = GuessA + GuessB + GuessC;
+  int GuessProduct = GuessA * GuessB * GuessC;
+
+  if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
+    std::cout << "You Win!" << std::endl;
+  } else {
+    std::cout << "You Lose!" << std::endl;
+  }
 
   return 0;
 }
